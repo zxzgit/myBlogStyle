7edaeba3c95a0a0b4abd211dc8865bd7a093eb4f
zxzgit <578226426@qq.com>
2017-01-09 18:50:38 +0800

    comment display

--- article2.php
+++ article2.php
@@ -35,13 +35,13 @@
 	<div class="content_main main_article">
 		<div class="content_header">
 			<a href="#" class="title">一二三四五六七八九十啊哦额</a>
-
+			
 			<div class="article_more_info">
 				阅读量：100000000
 				<a href="javascript:void(0);">收藏（500）</a>
 				<a href="javascript:void(0);">评论（500）</a>
 			</div>
-
+		
 		</div>
 		<div class="article_display">
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
@@ -100,14 +100,14 @@
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
 			氢气球、氦气球，再轻还是用重量，如果弄个硬质外壳的球形，内部有结构支撑不会被大气压压垮了，抽成真空，不是质量就是0了？大气浮力抵消壳子重量不就飞起来了？
-
+			
 			有无人用这种原理做什么实用化东西出来？做飞行器不好吗？
-
+			
 			还是有什么技术难点导致没法实现？
-
+			
 			===========
 			下面的资料有用吗？
-
+			
 			浙大造出世界最轻固体材料：压不弯狗尾巴草
 			最轻的物质―
 			=================
@@ -115,18 +115,18 @@
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？
 			举报 50 条评论 分享 • 邀请回答
 			查看全部 276 个回答
-
+			
 			8744
 			赞同反对，不会显示你的姓名
 			徐克化学话题优秀回答者 略懂略…
 			8744 人赞同
 			你们先别忙着嘲讽题主～其实题主问得好啊！
-
+			
 			问题的答案显然是：可行，完全可行，非常可行；唯一的问题是，即便题主预想中强大的能抵御大气压的外壳被开发出来，这个方案在地球上的宏观应用中也很难有任何经济上的优势。
-
+			
 			密度差是浮力的驱动力。如果使用氢气的话，这个密度差是空气的93%； 如果像楼主那样使用真空的话，这个密度差是空气的100%。也就是说，如果不考虑其他问题，楼主的方案
-
-
+			
+			
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
@@ -146,7 +146,7 @@
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
 			在真空状态将物质加工为海绵状，是否可提高强度而又不增加重量？<br>
-
+		
 		</div>
 	</div>
 	<div class="content_main more_article">
@@ -208,7 +208,7 @@
 			</li>
 		</ul>
 	</div>
-
+	
 	<div class="content_main article_comment">
 		<span class="content_header">评论区</span>
 		<ul class="article_list comment_list">
@@ -219,7 +219,7 @@
 						<a href="#" class="content_header_avatar">
 							<img src="img/avatar.png" style="width: 100%;height: 100%;">
 						</a>
-
+						
 						<div class="content_header_title">
 							<a href="#">zxz</a>
 						</div>
@@ -238,105 +238,71 @@
 							<div>评论(100)</div>
 						</div>
 					</div>
-
-				</div>
-
-			</li>
-			<li class="article_overview">
-				<div class="article_overview_wrap">
-					
-					<div class="content_header">
-						<a href="#" class="content_header_avatar">
-							<img src="img/avatar.png" style="width: 100%;height: 100%;">
-						</a>
-
-						<div class="content_header_title">
-							<a href="#">zxz</a>
-						</div>
-						<div class="content_add_time">2017-1-2 21:17:45</div>
-						<!--<img class="content_header_img" src="img/1.png">-->
-					</div>
-					<div class="preview_content">
-						<div class="preview_content_show">
-							讲道理代理费讲道理反垄断……
-						</div>
-						<div class="comment_op">
-							<div>赞</div>
-							<div>踩</div>
-							<div>回复</div>
-							<div>举报</div>
-							<div>评论(100)</div>
-						</div>
-					</div>
-
+				
 				</div>
 			</li>
+			
+			<!--评论迭代基本格式-->
 			<li class="article_overview">
 				<div class="article_overview_wrap">
-
+					<!--头部，评论用户信息、评论时间-->
 					<div class="content_header">
 						<a href="#" class="content_header_avatar">
-							<img src="img/avatar.png" style="width: 100%;height: 100%;">
+							<img src="img/avatar.png">
 						</a>
-
+						
 						<div class="content_header_title">
-							<a href="#">zxz</a>
+							<a href="#">ck</a>
 						</div>
-						<div class="content_add_time">2017-1-2 21:17:45</div>
-						<!--<img class="content_header_img" src="img/1.png">-->
+						<div class="content_add_time">2017年1月9日09:42:14</div>
 					</div>
+					<!--关联评论-->
+					<div class="relative_comment_list">
+						<div class="relative_comment">
+							<div class="relative_comment">
+								<p>
+									<a href="#">user1</a>
+									<a class="report_comment" href="#">举报</a>
+									<time>====2017-1-9 16:02:52</time>
+								</p>
+								<p>content1</p>
+								<p class="relative_comment_op">
+									<a href="#">顶(10000)</a>
+									<a href="#">踩(100)</a>
+									<a href="#">回复</a>
+									<a href="#">分享</a>
+								</p>
+							</div>
+							
+							<p>
+								<a href="#">user2</a>
+								<a class="report_comment" href="#">举报</a>
+								<time>====2017-1-9 16:02:52</time>
+							</p>
+							<p>content2</p>
+							<p class="relative_comment_op">
+								<a href="#">顶(10000)</a>
+								<a href="#">踩(100)</a>
+								<a href="#">回复</a>
+								<a href="#">分享</a>
+							</p>
+						</div>
+					</div>
+					<!--评论内容以及相关操作-->
 					<div class="preview_content">
 						<div class="preview_content_show">
-							反垄断法中的洛克菲勒都放假了的看法讲道理代理费讲道理反垄断法中的洛克菲勒都放假了的看法讲道理代理费讲道理反垄断法中的洛克菲勒都放假了的看法讲道理代理费讲道理反垄断……
+						this user comment content
 						</div>
 						<div class="comment_op">
-							<div>赞</div>
-							<div>踩</div>
-							<div>回复</div>
-							<div>举报</div>
-							<div>评论(100)</div>
-						</div>
-					</div>
-
-				</div>
-
-			</li>
-
-			<li class="article_overview">
-				<div class="article_overview_wrap">
-					<div class="content_header">
-						<a href="#" class="content_header_avatar">
-							<img src="img/avatar.png" style="width: 100%;height: 100%;">
-						</a>
-
-						<div class="content_header_title">
-							<a href="#"></a>
-						</div>
-						<div class="content_add_time"></div>
-						<!--<img class="content_header_img" src="img/1.png">-->
-					</div>
-					<div class="relative_comment_list jsShow">
-						<div>
-							wewe
-							<time></time>
-							<p>content</p>
-						</div>
-
-					</div>
-					<div class="preview_content">
-						<div class="preview_content_show">
-
-						</div>
-						<div class="comment_op">
-							<div>踩()</div>
-							<div>回复()</div>
+							<div>踩(0)</div>
+							<div>回复(0)</div>
 							<div>举报</div>
 						</div>
 					</div>
 				</div>
 			</li>
-
-
+		
+		
 		</ul>
 	</div>
 
@@ -361,58 +327,126 @@
 
 <script>
 	(function ($) {
-		var doAjax = function (callback) {
+		var doAjax       = function (callback) {
 				$.ajax({
-					url: 'getComment2.php',
-					data: {},
-					type: 'get',
+					url     : 'getComment2.php',
+					data    : {},
+					type    : 'get',
 					dataType: "json",
-					success: function (result) {
+					success : function (result) {
 						callback(result);
 						return true;
 					},
-					error: function () {
+					error   : function () {
 						alert('获取评论失败！');
 					}
 				});
 				return true;
 			},
-			getComment = function (page) {
+			getComment   = function (page) {
+				$('.comment_list').html('加载中...');
 				doAjax(function (result) {
 					var relativeCmidsList = result.relativeCmidsList,
-						commentsList = result.commentsList;
+						commentsList      = result.commentsList,
+						pageCommentHtml   = '';
 //					console.log(relativeCmidsList);
 					for (var cmIndex in relativeCmidsList) {
-						var cmidsStr = relativeCmidsList[cmIndex],
+						var cmidsStr  = relativeCmidsList[cmIndex],
 							cmidsList = cmidsStr.split(',').reverse();
-						console.log(cmidsList);
-
-						var mainCmid = cmidsList.shift(), showHtml = '', comment = '';
-
-						for (var i in cmidsList) {
+					
+						var mainCmid         = cmidsList.shift(),
+							showHtml         = '',
+							comment          = '',
+							thisCommentBlock = commentBlock.replace('{{username}}', commentsList[mainCmid]['from_uid'])
+								.replace('{{addtime}}', commentsList[mainCmid]['addtime'])
+								.replace('{{commentContent}}', commentsList[mainCmid]['content']);
+					
+					
+						for (var i = 0; i < cmidsList.length; i++) {
 							comment = commentsList[cmidsList[i]] ? commentsList[cmidsList[i]] : '';
-							if (comment) {
-								showHtml += '<div>' + comment['from_uid']
-									+ '---<time>' + comment['addtime'] + '</time>'
-									+ '<p>' + comment['content'] + '</p>';
+						
+							//当数据评论过多时，中奖的部分宽度100%，使其不会因为评论过多导致最顶端评论宽度十分狭窄
+							var middleStr = '';
+							if (cmidsList.length > 10 && i == 4) {
+								i++;
+								while (i < cmidsList.length - 3) {
+									var commentMiddle = commentsList[cmidsList[i]] ? commentsList[cmidsList[i]] : '';
+									middleStr += '<div style="width: 100%;border-width: 0;border-top-width: 1px;border-radius: 0px;">';
+									if (commentMiddle) {
+										middleStr += ''
+											+ '<p>'
+											+ commentMiddle['from_uid'] + '    <time>' + commentMiddle['addtime'] + '</time>'
+											+ '</p>'
+											+ '    <p>' + commentMiddle['content'] + '</p>';
+									} else {
+										middleStr += '<p>该评论已经删除<p>';
+									}
+									middleStr += '</div> ';
+								
+									i++;
+								}
 							}
-
+//							console.log(middleStr);
+							if (!comment) {
+								showHtml = '<div>' + showHtml + '<p>该评论已经删除</p>' + middleStr + '</div>';
+							} else {
+								showHtml = ''
+									+ '<div>'
+									+ showHtml
+									+ '    <p>' + comment['from_uid'] + '    <time>' + comment['addtime'] + '</time>' + '</p>'
+									+ '    <p>' + comment['content'] + '</p>'
+									+ middleStr
+									+ '</div>';
+							}
+						
+						
 						}
-						if (comment) {
-							showHtml += (new Array(cmidsList.length)).fill('</div>', 0, cmidsList.length).join('');
-						}
+						showHtml=showHtml?'<div class="relative_comment_list">'+showHtml+'</div>':showHtml;
+					
+						thisCommentBlock = thisCommentBlock.replace('{{relativeComment}}', showHtml);
 
-						console.log(showHtml);
-						$('.jsShow').html(showHtml);
+//						console.log(thisCommentBlock);
+						pageCommentHtml += thisCommentBlock;
 					}
-
-
+					$('.comment_list').html(pageCommentHtml?pageCommentHtml:'还没有评论。');
 				});
-			};
-
-
-		getComment();
-
+			},
+			commentBlock = '' +
+			'<li class="article_overview">' +
+			'   <div class="article_overview_wrap">' +
+			
+			'     <div class="content_header">' +
+			'        <a href="#" class="content_header_avatar">' +
+			'           <img src="img/avatar.png">' +
+			'        </a>' +
+			
+			'        <div class="content_header_title">' +
+			'           <a href="#">{{username}}</a>' +
+			'        </div>' +
+			'        <div class="content_add_time">{{addtime}}</div>' +
+			'     </div>' +
+			
+			'     {{relativeComment}}' +
+			
+			'     <div class="preview_content">' +
+			'        <div class="preview_content_show">{{commentContent}}</div>' +
+			'        <div class="comment_op">' +
+			'           <div>踩(0)</div>' +
+			'           <div>回复(0)</div>' +
+			'           <div>举报</div>' +
+			'        </div>' +
+			'     </div>' +
+			'   </div>' +
+			'</li>';
+		
+		
+//		getComment();
+		$('body').on('mouseover','.relative_comment',function () {
+			$('.relative_comment').removeClass('relative_comment_hover');
+			$(this).addClass('relative_comment_hover');
+		}).on('mouseout','.relative_comment',function () {
+			$('.relative_comment').removeClass('relative_comment_hover');
+		});
 	})(jQuery);
 </script>
 
--- getComment2.php
+++ getComment2.php
@@ -4,13 +4,14 @@
  * User: zxz
  * Datetime: 2017/1/6 17:25
  */
-//数据库存储先关评论从老到新，e.g  4,5,6代表：6是评论5的,5是评论4的
+//数据库存储先关评论从老到新，e.g.'6,5,4,' 代表：本条评论评论6，而6是评论5的,5是评论4的
+//执行添加的时候,如果是评论他人的都是：$from_cmids=所评论id+','+'所评论条目$from_cmids的值';
 
 $pdo = new PDO('mysql:dbname=yii2basic;host=127.0.0.1;charset=utf8', 'root', '');
 
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$stmt = $pdo->prepare('SELECT * FROM zxz_comment WHERE aid=1 AND cmid!=7');
+$stmt = $pdo->prepare('SELECT * FROM zxz_comment WHERE aid=1');
 $stmt->execute();//这里的$name 不要用 addslashed()处理,excute拼接字符串的时候好像在函数内对各个参数都进行了处理。
 $commentList = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -22,7 +23,7 @@ $commentByIdList   = [];//本业所有相关评论数组，键为评论id
 
 $relativeUids      = [];//相关评论用户
 foreach ($commentList as $commentListKey => $commentListVal) {
-	$relativeCmidsList[]=$commentListVal['from_cmids']?$commentListVal['from_cmids'].",{$commentListVal['cmid']}":$commentListVal['cmid'].'';
+	$relativeCmidsList[]=$commentListVal['from_cmids']?$commentListVal['from_cmids']."{$commentListVal['cmid']}":$commentListVal['cmid'].'';
 	$relativeUids[]=$commentListVal['from_uid'];
 
 	$commentByIdList[$commentListVal['cmid']]=$commentListVal;
@@ -32,10 +33,10 @@ foreach ($commentList as $commentListKey => $commentListVal) {
 
 //从数据库中获取未其他相关评论信息
 $fromCmids=array_diff(array_unique(explode(',',implode(',',$relativeCmidsList))),array_keys($commentByIdList));
+$fromCmids=array_filter($fromCmids);//去除空值
 
 //print_r($fromCmids);
-
-if (!empty($relativeCmidsList)) {
+if (!empty($fromCmids)) {
 	//查询自评论
 	$stmt = $pdo->prepare('SELECT * FROM zxz_comment WHERE cmid IN(' . implode(',', $fromCmids) . ')');
 	$stmt->execute();
@@ -50,6 +51,9 @@ if (!empty($relativeCmidsList)) {
 
 //print_r($commentByIdList);
 //print_r($relativeCmidsList);
+//获取评论用户信息
+
+
 
 $returnList=[
 	'commentsList'=>$commentByIdList,
